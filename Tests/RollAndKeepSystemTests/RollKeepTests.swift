256834d1ed7902616782c41246c307b8531b3c78
Micah A. Walles <micah@walles.info>
2021-02-21 17:05:01 -0600

    Updating reroll mechanic

--- Tests/RollAndKeepSystemTests/RollKeepTests.swift
+++ Tests/RollAndKeepSystemTests/RollKeepTests.swift
@@ -5,49 +5,49 @@ import XCTest
 
 final class RollKeepTests: XCTestCase {
 	func test5k3Pool() {
-		let pool = RollAndKeepRoll(name: "Test Roll", roll: 5, keep: 3)
+		let pool = RollAndKeepRoll(roll: 5, keep: 3)
 		XCTAssertEqual(pool.pool.dice.count, 5)
 		XCTAssertEqual(pool.diceActuallyKept, 3)
 		XCTAssertEqual(pool.rollBonus, 0)
 	}
 
 	func test10k10Pool() {
-		let pool = RollAndKeepRoll(name: "Test Roll", roll: 10, keep: 10)
+		let pool = RollAndKeepRoll(roll: 10, keep: 10)
 		XCTAssertEqual(pool.pool.dice.count, 10)
 		XCTAssertEqual(pool.diceActuallyKept, 10)
 		XCTAssertEqual(pool.rollBonus, 0)
 	}
 
 	func test11k4Pool() {
-		let pool = RollAndKeepRoll(name: "Test Roll", roll: 11, keep: 4)
+		let pool = RollAndKeepRoll(roll: 11, keep: 4)
 		XCTAssertEqual(pool.pool.dice.count, 10)
 		XCTAssertEqual(pool.diceActuallyKept, 4)
 		XCTAssertEqual(pool.rollBonus, 0)
 	}
 
 	func test14k4Pool() {
-		let pool = RollAndKeepRoll(name: "Test Roll", roll: 14, keep: 4)
+		let pool = RollAndKeepRoll(roll: 14, keep: 4)
 		XCTAssertEqual(pool.pool.dice.count, 10)
 		XCTAssertEqual(pool.diceActuallyKept, 6)
 		XCTAssertEqual(pool.rollBonus, 0)
 	}
 
 	func test13k9Pool() {
-		let pool = RollAndKeepRoll(name: "Test Roll", roll: 13, keep: 9)
+		let pool = RollAndKeepRoll(roll: 13, keep: 9)
 		XCTAssertEqual(pool.pool.dice.count, 10)
 		XCTAssertEqual(pool.diceActuallyKept, 10)
 		XCTAssertEqual(pool.rollBonus, 2)
 	}
 
 	func test10k12Pool() {
-		let pool = RollAndKeepRoll(name: "Test Roll", roll: 10, keep: 12)
+		let pool = RollAndKeepRoll(roll: 10, keep: 12)
 		XCTAssertEqual(pool.pool.dice.count, 10)
 		XCTAssertEqual(pool.diceActuallyKept, 10)
 		XCTAssertEqual(pool.rollBonus, 4)
 	}
 
 	func test16k12Pool() {
-		let pool = RollAndKeepRoll(name: "Test Roll", roll: 16, keep: 12)
+		let pool = RollAndKeepRoll(roll: 16, keep: 12)
 		XCTAssertEqual(pool.pool.dice.count, 10)
 		XCTAssertEqual(pool.diceActuallyKept, 10)
 		XCTAssertEqual(pool.rollBonus, 16)
